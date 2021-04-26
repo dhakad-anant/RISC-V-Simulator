3693e390cee4df21b7957fd41f88bd11482d56b2
jaglikemakkar <jaglike.makkar@gmail.com>
2021-04-26 14:43:21 +0530

    registers updated

--- Phase2/practice.py
+++ Phase2/practice.py
@@ -1,1 +1,3 @@
-print(int('1100111',2))
+for i in range (16):
+    # self.reg1.setText('x0:' +str(ProcessingUnit.reg[0]))
+    print("self.reg"+str(i+1)+".setText('x"+str(i+16)+": ' + str(ProcessingUnit.reg["+str(i+16)+"]))")
